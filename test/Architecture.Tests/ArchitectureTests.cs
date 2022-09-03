748a2b3df3e35b0fb0e6cd4084263ae4586c5475
Marina Sundström <marna.li@outlook.com>
2022-09-03 18:10:45 +0200

    Add Architecture Tests

---
+++ test/Architecture.Tests/ArchitectureTests.cs
@@ -0,0 +1,147 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace Architecture.Tests;
+
+public class ArchitectureTests
+{
+    private const string DomainNamespace = "TodoApp.Domain";
+    private const string ApplicationNamespace = "TodoApp.Application";
+    private const string InfrastructureNamespace = "TodoApp.Infrastructure";
+    private const string PresentationNamespace = "TodoApp.Presentation";
+    private const string WebAppNamespace = "TodoApp.WebApp";
+
+    [Fact]
+    public void Domain_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(TodoApp.Domain.Extensions).Assembly;
+
+        var otherProjects = new[]
+        {
+            ApplicationNamespace,
+            InfrastructureNamespace,
+            PresentationNamespace,
+            WebAppNamespace
+        };
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Application_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(TodoApp.Application.ServiceExtensions).Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            PresentationNamespace,
+            WebAppNamespace
+        };
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Handlers_Should_Have_DependencyOnDomain()
+    {
+        // Arrange
+        var assembly = typeof(TodoApp.Application.ServiceExtensions).Assembly;
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .That()
+            .HaveNameEndingWith("Handler")
+            .Should()
+            .HaveDependencyOn(DomainNamespace)
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Infrastructure_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(TodoApp.Application.ServiceExtensions).Assembly;
+
+        var otherProjects = new[]
+        {
+            PresentationNamespace,
+            WebAppNamespace
+        };
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Presentation_Should_Not_HaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = typeof(TodoApp.Application.ServiceExtensions).Assembly;
+
+        var otherProjects = new[]
+        {
+            InfrastructureNamespace,
+            PresentationNamespace,
+            WebAppNamespace
+        };
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Controllers_Should_Not_HaveDependencyOnMediatR()
+    {
+        // Arrange
+        var assembly = typeof(TodoApp.Presentation.ServiceExtensions).Assembly;
+
+        // Act
+        var testResult = Types
+            .InAssembly(assembly)
+            .That()
+            .HaveNameEndingWith("Controller")
+            .Should()
+            .HaveDependencyOn("MediatR")
+            .GetResult();
+
+        // Assert
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+}
