5aeaaa71a7f48e1929245756103a478fe1cb941d
Marina Sundström <marna.li@outlook.com>
2022-09-03 18:28:10 +0200

    Add Domain Tests

---
+++ test/Domain.Tests/TodoTest.cs
@@ -0,0 +1,70 @@
+﻿using TodoApp.Domain.Entities;
+using TodoApp.Domain.Enums;
+using TodoApp.Domain.Events;
+
+namespace Domain.Tests;
+
+public class TodoTest
+{
+    [Fact]
+    public void CreateTodo()
+    {
+        var todo = new Todo("Foo", "Bar", TodoApp.Domain.Enums.TodoStatus.New);
+
+        Assert.Single(todo.DomainEvents.OfType<TodoCreated>());
+    }
+
+    [Fact]
+    public void UpdateTitle()
+    {
+        // Arrange
+        var todo = new Todo("Foo", "Bar", TodoApp.Domain.Enums.TodoStatus.New);
+
+        var newTitle = "Zack";
+
+        // Act
+        todo.UpdateTitle(newTitle);
+
+        // Assert
+        Assert.Equal(newTitle, todo.Title);
+
+        Assert.Single(todo.DomainEvents.OfType<TodoTitleUpdated>());
+        Assert.Single(todo.DomainEvents.OfType<TodoUpdated>());
+    }
+
+    [Fact]
+    public void UpdateDescription()
+    {
+        // Arrange
+        var todo = new Todo("Foo", "Bar", TodoApp.Domain.Enums.TodoStatus.New);
+
+        var newDescription = "This is a new description";
+
+        // Act
+        todo.UpdateDescription(newDescription);
+
+        // Assert
+        Assert.Equal(newDescription, todo.Description);
+
+        Assert.Single(todo.DomainEvents.OfType<TodoDescriptionUpdated>());
+        Assert.Single(todo.DomainEvents.OfType<TodoUpdated>());
+    }
+
+    [Fact]
+    public void UpdateStatus()
+    {
+        // Arrange
+        var todo = new Todo("Foo", "Bar", TodoApp.Domain.Enums.TodoStatus.New);
+
+        var newStatus = TodoStatus.Completed;
+
+        // Act
+        todo.UpdateStatus(newStatus);
+
+        // Assert
+        Assert.Equal(TodoStatus.Completed, todo.Status);
+
+        Assert.Single(todo.DomainEvents.OfType<TodoStatusUpdated>());
+        Assert.Single(todo.DomainEvents.OfType<TodoUpdated>());
+    }
+}
