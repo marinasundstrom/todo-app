eb1acd0927a1902f1914e3ae830e3095b0771736
Marina Sundström <marna.li@outlook.com>
2022-09-04 21:49:46 +0200

    Add MassTransit tests to IntegrationTests

---
+++ test/IntegrationTests/TodosTest.MassTransit.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MassTransit;
+using MassTransit.Testing;
+using TodoApp.Consumers;
+using TodoApp.Contracts;
+using static MassTransit.Logging.OperationName;
+
+namespace TodoApp.IntegrationTests;
+
+partial class TodosTest : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    [Fact]
+    public async Task UpdateStatusConsumed()
+    {
+        // Arrange
+
+        var harness = _factory.Services.GetTestHarness();
+
+        await harness.Start();
+
+        var client = _factory.CreateClient();
+
+        TodosClient todosClient = new(client);
+
+        string title = "Foo Bar";
+        string description = "Lorem ipsum";
+        TodoStatusDto status = TodoStatusDto.Ongoing;
+
+        var newStatus = TodoStatusDto.Completed;
+
+        var todo = await todosClient.CreateTodoAsync(new CreateTodoRequest()
+        {
+            Title = title,
+            Description = description,
+            Status = status
+        });
+
+        // Act
+
+        await harness.Bus.Publish(
+            new UpdateStatus( todo.Id, (Contracts.TodoStatusDto)newStatus));
+
+        // Assert
+
+        Assert.True(await harness.Consumed.Any<UpdateStatus>());
+
+        var todo2 = await todosClient.GetTodoByIdAsync(todo.Id);
+
+        todo2.Status.Should().Be(newStatus);
+    }
+}
+
