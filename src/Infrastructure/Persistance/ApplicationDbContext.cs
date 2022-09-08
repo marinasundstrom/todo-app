84665ea6e98ab17b80ddb73caa59929feaded2e4
Marina Sundström <marna.li@outlook.com>
2022-09-08 22:46:17 +0200

    Handle soft delete

--- src/Infrastructure/Persistance/ApplicationDbContext.cs
+++ src/Infrastructure/Persistance/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using TodoApp.Infrastructure.Persistance.Interceptors;
+﻿using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
+using TodoApp.Infrastructure.Persistance.Interceptors;
 
 namespace TodoApp.Infrastructure.Persistance
 {
@@ -28,12 +29,39 @@ namespace TodoApp.Infrastructure.Persistance
         {
             base.OnModelCreating(modelBuilder);
 
+            ApplySoftDeleteQueryFilter(modelBuilder);
+
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        private static void ApplySoftDeleteQueryFilter(ModelBuilder modelBuilder)
+        {
+            // INFO: This code adds a query filter to any object deriving from Entity
+            //       and that is implementing the ISoftDelete interface.
+            //       The generated expressions correspond to: (e) => e.Deleted == null.
+            //       Causing the entity not to be included in the result if Deleted is not null.
+            //       There are other better ways to approach non-destructive "deletion".
+
+            var entityBaseType = typeof(Entity);
+            var softDeleteInterface = typeof(ISoftDelete);
+            var deletedProperty = softDeleteInterface.GetProperty(nameof(ISoftDelete.Deleted));
+
+            foreach (var entityType in softDeleteInterface.Assembly
+                .GetTypes()
+                .Where(candidateEntityType => entityBaseType.IsAssignableFrom(candidateEntityType))
+                .Where(candidateEntityType => softDeleteInterface.IsAssignableFrom(candidateEntityType)))
+            {
+                var param = Expression.Parameter(entityType, "entity");
+                var body = Expression.Equal(Expression.Property(param, deletedProperty!), Expression.Constant(null));
+                var lambda = Expression.Lambda(body, param);
+
+                modelBuilder.Entity(entityType).HasQueryFilter(lambda);
+            }
         }
 
 #nullable disable
 
-    public DbSet<Todo> Todos { get; set; }
+        public DbSet<Todo> Todos { get; set; }
 
 #nullable restore
 
