18f0341265876093a99792696c8c7731789afe6e
Marina Sundström <marna.li@outlook.com>
2022-10-17 22:25:21 +0200

    Update packages

--- src/Application/Behaviors/ValidationBehavior.cs
+++ src/Application/Behaviors/ValidationBehavior.cs
@@ -11,7 +11,7 @@ public sealed class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<
 
     public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) => _validators = validators;
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (!_validators.Any())
         {
