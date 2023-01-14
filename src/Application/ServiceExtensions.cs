0f685e27db7ec0c40cad0d59d85c37f2286c1df8
Marina Sundström <marna.li@outlook.com>
2023-01-14 11:27:43 +0100

    Add LoggingBehavior

--- src/Application/ServiceExtensions.cs
+++ src/Application/ServiceExtensions.cs
@@ -10,6 +10,7 @@ public static class ServiceExtensions
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(typeof(ServiceExtensions));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
         services.AddValidatorsFromAssembly(typeof(ServiceExtensions).Assembly);
