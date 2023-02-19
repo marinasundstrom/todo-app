69c8af6e67387ad1a32f65ba572c6c60601acd52
Marina Sundström <marna.li@outlook.com>
2023-02-19 18:30:47 +0100

    Fix guarded entity Id types

--- src/Application/ValueObjects/TodoId.cs
+++ src/Application/ValueObjects/TodoId.cs
@@ -1,15 +1,31 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace TodoApp.Application.ValueObjects;
 
-public readonly struct TodoId
+public struct TodoId
 {
     public TodoId(int value) => Value = value;
 
-    public int Value { get; }
+    public int Value { get; set; }
+
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
 
     public override string ToString()
     {
         return Value.ToString();
     }
+
+    public static bool operator ==(TodoId lhs, TodoId rhs) => lhs.Value == rhs.Value;
+
+    public static bool operator !=(TodoId lhs, TodoId rhs) => lhs.Value != rhs.Value;
 
     public static implicit operator TodoId(int id) => new TodoId(id);
 
