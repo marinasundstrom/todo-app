c32fb9be43cc3c29215bc4fe486d11f84da8be28
Marina Sundström <marna.li@outlook.com>
2023-02-24 11:58:33 +0100

    Update MediatR

--- src/Web/Extensions/ServiceExtensions.cs
+++ src/Web/Extensions/ServiceExtensions.cs
@@ -9,7 +9,7 @@ public static class ServiceExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(typeof(ServiceExtensions));
+        services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining(typeof(ServiceExtensions)));
         services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
